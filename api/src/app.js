7bf0c1c140ee19acb2116a65197a3c40ec377788
Rodrigo Godoy <contato@rodrigogodoy.net>
2021-07-26 19:04:15 -0300

    docker com mongo e conexao no db

--- api/src/app.js
+++ api/src/app.js
@@ -1,11 +1,38 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import cors from 'cors'
 import routes from './routes'
 
-const app = express()
-const port = 5400
+dotenv.config()
 
-app.use('/', routes)
+class App {
+  constructor() {
+    this.express = express()
+    this.middlewares()
+    this.database()
+    this.routes()
+  }
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  middlewares() {
+    this.express.use(express.json())
+    this.express.use(cors())
+  }
+
+  database() {
+    const user = process.env.DB_USER
+    const password = process.env.DB_PASSWORD
+    const host = process.env.DB_HOST
+    const name = process.env.DB_NAME
+    mongoose.connect(`mongodb://${user}:${password}@${host}/${name}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  }
+
+  routes() {
+    this.express.use(routes)
+  }
+}
+
+export default new App().express
--- api/src/routes.js
+++ api/src/routes.js
@@ -2,7 +2,7 @@ import { Router } from 'express'
 const routes = Router()
 
 routes.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Bem-vindo')
 })
 
 export default routes
