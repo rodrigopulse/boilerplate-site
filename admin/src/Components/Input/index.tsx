0ee41d0e105a8861c1cd5a86a861ae9f12b23cc4
Rodrigo Godoy <contato@rodrigogodoy.net>
2021-08-27 16:25:37 -0300

    type password para a senha

--- admin/src/Components/Input/index.tsx
+++ admin/src/Components/Input/index.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import './styles.scss'
-import {InputProps} from './types'
+import { InputProps } from './types'
 
-export const Input: React.FC<InputProps> = ({ placeholder, type, onChange }) => {
+export const Input: React.FC<InputProps> = ({
+  placeholder,
+  type,
+  onChange
+}) => {
   const onChangeReturn = (e: any) => {
     let element = e.target.value
-    if(!element) {
+    if (!element) {
       element = ''
     }
     onChange(element)
   }
-  return <input className='input' placeholder={ placeholder } type={ type } onChange={ (e) => onChangeReturn(e) } />
+  return (
+    <input
+      className="input"
+      placeholder={placeholder}
+      type={type}
+      onChange={(e) => onChangeReturn(e)}
+    />
+  )
 }
--- admin/src/views/Login/index.tsx
+++ admin/src/views/Login/index.tsx
@@ -3,13 +3,23 @@ import { Container, Input } from '../../Components'
 import './styles.scss'
 
 const Login: React.FC = () => {
-  const [ user, setUser ] = useState('')
-  const [ password, setPassword ] = useState('')
+  const [user, setUser] = useState('')
+  const [password, setPassword] = useState('')
   return (
     <Container full justify="center" align="center" background="secondary">
       <div className="login__content">
-        <Input placeholder="Usuário" onChange={ (e) => setUser(e) } value={ user } />
-        <Input placeholder="Senha" onChange={ (e) => setPassword(e) } value={ password }/>
+        <Input
+          placeholder="Usuário"
+          onChange={(e) => setUser(e)}
+          value={user}
+          type="text"
+        />
+        <Input
+          placeholder="Senha"
+          onChange={(e) => setPassword(e)}
+          value={password}
+          type="password"
+        />
       </div>
     </Container>
   )
